ccdd4294d455df80af87fdaa2541eb9fd1deb6cb
ldseraph <ld.seraph@163.com>
2022-07-26 11:07:40 +0800

    feat: :sparkles: add wdt thread

--- app/main.c
+++ app/main.c
@@ -2,8 +2,69 @@
 #define DBG_SECTION_NAME "main"
 #include <ulog.h>
 
+#define WDT_THREAD_STACK_SIZE 1024
+#define WDT_THREAD_DELAY_MS 10000
+
+static rt_thread_t wdt_thread;
+static rt_timer_t  wdt_timer;
+static rt_sem_t    wdt_sem;
+
+static void tension_app_thread_handle(void* param) {
+  for (;;) {
+    rt_sem_take(wdt_sem, RT_WAITING_FOREVER);
+    LOG_I("board reload");
+    board_fwd_reload();
+  }
+  return;
+}
+
+static void wdt_timer_handle(void* param) {
+  rt_interrupt_enter();
+  rt_sem_release(wdt_sem);
+  rt_interrupt_leave();
+}
+
 int main() {
   rt_err_t err;
+
+  wdt_sem = rt_sem_create("wdt", 1, RT_IPC_FLAG_PRIO);
+  if (wdt_sem == RT_NULL) {
+    LOG_E("wdt_sem err");
+    return -RT_ERROR;
+  }
+
+  wdt_thread = rt_thread_create("wdt",
+                                tension_app_thread_handle,
+                                RT_NULL,
+                                WDT_THREAD_STACK_SIZE,
+                                30,
+                                20);
+  if (wdt_thread == RT_NULL) {
+    LOG_E("rt_thread_create err");
+    return -RT_ERROR;
+  }
+
+  err = rt_thread_startup(wdt_thread);
+  if (err != RT_EOK) {
+    LOG_E("rt_thread_startup err: %d", err);
+    return err;
+  }
+
+  wdt_timer = rt_timer_create("wdt",
+                              wdt_timer_handle,
+                              RT_NULL,
+                              rt_tick_from_millisecond(WDT_THREAD_DELAY_MS),
+                              RT_TIMER_FLAG_PERIODIC);
+  if (wdt_timer == RT_NULL) {
+    LOG_E("wdt_timer err: %d", err);
+    return RT_ERROR;
+  }
+
+  err = rt_timer_start(wdt_timer);
+  if (err != RT_EOK) {
+    LOG_E("wdt_timer: %d", err);
+    return err;
+  }
 
 #ifdef RT_USING_LORAWAN
   err = lorawan_app();
